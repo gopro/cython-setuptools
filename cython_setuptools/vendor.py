757cad12d41c53f93f5ae4082191d63ee71490c5
Marco Treglia <mtreglia@gopro.com>
2024-02-27 15:42:28 +0100

    vendor: remove cpp_str and tags after usage
    This will prevent this type of warning:
    
    .sx/stupeflix/lib/python3.10/site-packages/setuptools/_distutils/extension.py:134:
     UserWarning: Unknown Extension options: 'cpp_std', 'tags' warnings.warn(msg)
    
    It's wrong to forward the keyword 'cpp_std' and 'tags' to
    cython.Extension and the latter is used to fill the
    extra_compile_args in:
    
    https://github.com/gopro/cython-setuptools/blob/main/cython_setuptools/vendor.py#L295-L296

--- cython_setuptools/vendor.py
+++ cython_setuptools/vendor.py
@@ -203,6 +203,11 @@ def create_cython_ext_modules(cython_modules, profile_cython=False, debug=False)
                 args = kwargs.setdefault(args_name, [])
                 if "-g" not in args:
                     args.append("-g")
+        # Remove custom cython_setuptools options
+        if "cpp_std" in kwargs:
+            del kwargs["cpp_std"]
+        if "tags" in kwargs:
+            del kwargs["tags"]
         ext = Extension(**kwargs)
         ret.append(ext)
     return ret
